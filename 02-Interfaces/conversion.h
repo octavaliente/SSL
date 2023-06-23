8a89805e0b1f62416a57e871bb5e9efa3d7bb85f
octavaliente <107971690+octavaliente@users.noreply.github.com>
2023-06-23 15:11:41 -0300

    Delete 02-Interfaces directory

--- 02-Interfaces/conversion.h
+++
@@ -1,8 +0,0 @@
-//convertidor celsius a fahrenheit
-#ifndef __CONVERTIDOR_H__
-#define __CONVERTIDOR_H__
-
-double fahrenheit (double);
-double celsius (double);
-
-#endif
--- 02-Interfaces/conversion_tests.c
+++
@@ -1,8 +0,0 @@
-#include <assert.h>
-#include "conversion.h"
-
-int main (void){
-    assert(fahrenheit(100)==fahrenheit(celsius(fahrenheit(100))));
-    assert(celsius(100)==celsius(fahrenheit(celsius(100))));
-    return 0;
-}
